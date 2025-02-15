a98d8cfeb0dda21b12f5d0b7d143eb65103c608e
João Simões <gravity00@users.noreply.github.com>
2025-02-15 23:29:26 +0000

    manual external init class definition for older framework versions

---
+++ IsExternalInit.cs
@@ -0,0 +1,11 @@
+﻿#if !NET50_OR_GREATER
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices;
+
+[Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[Diagnostics.DebuggerNonUserCode]
+internal static class IsExternalInit
+{
+
+}
+#endif
