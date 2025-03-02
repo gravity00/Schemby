f2c7bb008128b595a84ff5d1bde7d11b6fc92793
João Simões <gravity00@users.noreply.github.com>
2025-03-02 23:16:14 +0000

    defined column properties

--- src/Schemby.Core/Specifications/Column.cs
+++ src/Schemby.Core/Specifications/Column.cs
@@ -5,11 +5,30 @@
 /// </summary>
 /// <param name="Name">The column name</param>
 /// <param name="Type">The column type</param>
+/// <param name="Nullable">Is the column nullable?</param>
+/// <param name="Length">The column length</param>
 public record Column(
     string Name,
-    ColumnType Type
+    ColumnType Type,
+    bool Nullable,
+    int Length
 )
 {
+    /// <summary>
+    /// The column precision.
+    /// </summary>
+    public int? Precision { get; init; }
+
+    /// <summary>
+    /// The column scale.
+    /// </summary>
+    public int? Scale { get; init; }
+
+    /// <summary>
+    /// The column default value.
+    /// </summary>
+    public string? DefaultValue { get; init; }
+
     /// <summary>
     /// Specification description.
     /// </summary>
