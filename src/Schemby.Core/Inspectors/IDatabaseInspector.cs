f162d66f1c3eacb6d8c33d514b36cfe6f13e492a
João Simões <gravity00@users.noreply.github.com>
2025-03-02 23:51:33 +0000

    defined interfaces for DB inspectors

---
+++ src/Schemby.Core/Inspectors/IDatabaseInspector.cs
@@ -0,0 +1,24 @@
+﻿using Schemby.Specifications;
+
+namespace Schemby.Inspectors;
+
+/// <summary>
+/// Interface for database inspectors.
+/// </summary>
+public interface IDatabaseInspector
+{
+    /// <summary>
+    /// Inspects the database and returns a database specification.
+    /// </summary>
+    /// <param name="database">The database name to inspect</param>
+    /// <param name="tableFilter">An optional table filter</param>
+    /// <param name="columnFilter">An optional column filter</param>
+    /// <param name="ct">A token to cancel the operation</param>
+    /// <returns>The database specification</returns>
+    Task<Database> InspectAsync(
+        string database,
+        string? tableFilter,
+        string? columnFilter,
+        CancellationToken ct
+    );
+}
---
+++ src/Schemby.Core/Inspectors/IDatabaseInspectorFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace Schemby.Inspectors;
+
+/// <summary>
+/// Interface for database inspector factories.
+/// </summary>
+public interface IDatabaseInspectorFactory
+{
+    /// <summary>
+    /// Creates a database inspector.
+    /// </summary>
+    /// <param name="provider">The provider selector alias</param>
+    /// <param name="connectionString">The connection string to be used by the inspector</param>
+    /// <returns>The database inspector</returns>
+    IDatabaseInspector Create(
+        string provider,
+        string connectionString
+    );
+}
