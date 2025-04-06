dbedb43e56c7ef9ff0cb56d73ceba6cd9858e34c
João Simões <gravity00@users.noreply.github.com>
2025-04-06 23:17:27 +0100

    helper extensions to add core services

---
+++ src/Schemby/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Schemby;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods for <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds Schemby services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection</param>
+    /// <returns>A reference to this instance after the operation has completed</returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IServiceCollection AddSchemby(this IServiceCollection services)
+    {
+        if (services == null) throw new ArgumentNullException(nameof(services));
+
+        services.AddSingleton<ISqlRunner, SqlRunner>();
+        services.AddSingleton<IInspectorFactory, InspectorFactory>();
+
+        return services;
+    }
+}
