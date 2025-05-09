b44ab1dc47aaac98b961c8c63b1cb471a3f05f65
João Simões <gravity00@users.noreply.github.com>
2025-05-09 22:22:32 +0100

    defined serializer contracts

---
+++ src/Schemby.Contracts/ISerializer.cs
@@ -0,0 +1,22 @@
+﻿using Schemby.Specifications;
+
+namespace Schemby;
+
+/// <summary>
+/// Interface for specification serializers.
+/// </summary>
+public interface ISerializer
+{
+    /// <summary>
+    /// Serializes a specification into a stream.
+    /// </summary>
+    /// <param name="specification">The specification to serialize</param>
+    /// <param name="stream">The destination stream</param>
+    /// <param name="ct">The cancellation token</param>
+    /// <returns>A task to be awaited for the result</returns>
+    Task SerializeToStreamAsync(
+        Specification specification,
+        Stream stream,
+        CancellationToken ct
+    );
+}
---
+++ src/Schemby.Contracts/ISerializerFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Schemby;
+
+/// <summary>
+/// Interface for specification serializer factories.
+/// </summary>
+public interface ISerializerFactory
+{
+    /// <summary>
+    /// Creates a specification serializer.
+    /// </summary>
+    /// <param name="format">The serialization format</param>
+    /// <returns>The specification serializer</returns>
+    ISerializer Create(string format);
+}
